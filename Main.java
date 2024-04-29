1860f19821a57d8860d2a6a7e1288abc95be220d
Sasiingit <114077036+Sasiingit@users.noreply.github.com>
2024-04-29 11:35:16 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+class Main
+{
+  public static void main(String []args)
+  {
+    System.out.println("Hello World");
+    int a=10;
+    int b=20;
+    int c=a+b;
+    System.out.println("Result of addition is"+c);
+  }
+}
